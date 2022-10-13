2b800e61d324cf0b3e5487aff197bbd435b8cad6
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-10-13 11:05:00 +0700

    done likes

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -6,27 +6,37 @@ import useAuthStore from '../store/authStore'
 interface IPprops {
     handleLike: () => void
     handleDislike: () => void
-    likes:any[]
+    likes: any[]
 }
 
 
-const LikeButton = ({ likes, handleLike, handleDislike}: IPprops) => {
-    const [alreadyLike, setAlreadyLike] = useState(true)
-    const { userProfile } = useAuthStore()
+const LikeButton = ({ likes, handleLike, handleDislike }: IPprops) => {
+    const [alreadyLike, setAlreadyLike] = useState(false)
+    const { userProfile }: any = useAuthStore()
+    const filterLikes = likes?.filter((item) => item._ref === userProfile?._id)
+
+
+    useEffect(() => {
+        if (filterLikes?.length > 0) {
+            setAlreadyLike(true)
+        } else {
+            setAlreadyLike(false)
+        }
+    }, [filterLikes, likes])
 
     return (
-        <div className='gap-6'>
+        <div className='flex gap-6'>
             <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
-            {alreadyLike ? (
-                <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997]' onClick={handleDislike}>
-                    <MdFavorite className='text-lg md:text-2xl'/>
-                </div>
-            ) : (
-                <div className='bg-primary rounded-full p-2 md:p-4' onClick={handleLike}>
-                    <MdFavorite className='text-lg md:text-2xl'/>
-                </div>
-            )}
-            <p className='text-md font-semibold'>likes?.length || 0</p>
+                {alreadyLike ? (
+                    <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997]' onClick={handleDislike}>
+                        <MdFavorite className='text-lg md:text-2xl' />
+                    </div>
+                ) : (
+                    <div className='bg-primary rounded-full p-2 md:p-4' onClick={handleLike}>
+                        <MdFavorite className='text-lg md:text-2xl' />
+                    </div>
+                )}
+                <p className='text-md font-semibold'>{likes?.length || 0}</p>
             </div>
         </div>
     )
